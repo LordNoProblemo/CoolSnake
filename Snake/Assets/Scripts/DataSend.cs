d03e00650b15b0148663eb0fc7b776deaf2892e5
Omri Klein <omrik2007@gmail.com>
2016-11-07 14:47:47 +0200

    The Game

---
+++ Snake/Assets/Scripts/DataSend.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DataSend : Photon.MonoBehaviour {
+    public GameObject snake;
+    public int cnt=0;
+    public bool en=true;
+	// Use this for initialization
+	void Start () {
+        PhotonNetwork.sendRate = 50;
+        PhotonNetwork.sendRateOnSerialize = 50;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        en = snake.GetComponent<Snake>().enabled;
+        cnt = snake.GetComponent<Snake>().count;
+
+    }
+    void OnCollisionStay(Collision col)
+    {
+        if (PhotonNetwork.connected && col.gameObject.GetComponent<GameManager>() != null)
+        {
+            col.gameObject.GetComponent<GameManager>().otherCnt = cnt;
+            col.gameObject.GetComponent<GameManager>().En = en;
+        }
+    }
+    void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.isWriting)
+        {
+            stream.SendNext(en);
+            stream.SendNext(cnt);
+
+        }
+        else
+        {
+            // Network player, receive data
+            en = (bool)stream.ReceiveNext();
+            cnt = (int)stream.ReceiveNext();
+        }
+    }
+}
---
+++ Snake/Assets/Scripts/Enter.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enter : Photon.MonoBehaviour {
+
+    bool woof = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnCollisionStay(Collision other)
+    {
+        if (woof)
+            return;
+        woof = true;
+        Debug.Log(other.gameObject.GetComponentInParent<GameManager>() == null);
+        if (other.gameObject.GetComponent<GameManager>() == null && other.gameObject.GetComponentInParent<GameManager>()==null)
+            return;
+        if (other.gameObject.GetComponent<GameManager>() != null)
+        {
+            other.gameObject.GetComponent<GameManager>().snake2 = gameObject;
+            gameObject.GetComponent<Snake>().My = other.gameObject.GetComponent<GameManager>().PointsMul;
+            gameObject.GetComponent<Snake>().MullOther = other.gameObject.GetComponent<GameManager>().PointsMul;
+            gameObject.GetComponent<Snake>().enabled = true;
+            
+            other.gameObject.GetComponent<GameManager>().enabled = true;
+          
+            gameObject.transform.position = new Vector3(transform.position.x, 0.05f, transform.position.z);
+        }
+        else
+        {
+        
+            other.gameObject.GetComponentInParent<GameManager>().snake2 = gameObject;
+            gameObject.GetComponent<Snake>().My = other.gameObject.GetComponentInParent<GameManager>().PointsMul;
+            gameObject.GetComponent<Snake>().MullOther = other.gameObject.GetComponentInParent<GameManager>().PointsMul;
+            gameObject.GetComponent<Snake>().enabled = true;
+            //other.gameObject.GetComponent<GameManager>().ForceStart();
+            other.gameObject.GetComponentInParent<GameManager>().enabled = true;
+            gameObject.transform.position = new Vector3(transform.position.x, 0.05f, transform.position.z);
+
+        }
+
+
+    }
+}
