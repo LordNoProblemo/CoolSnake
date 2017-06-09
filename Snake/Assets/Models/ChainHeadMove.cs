5a1cf996c3a79850f55db92f2cb5e9ed5fa7c71a
Omri Klein <omrik2007@gmail.com>
2017-06-09 21:30:42 +0300

    Changes
    idk

---
+++ Snake/Assets/Models/ChainHeadMove.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChainHeadMove : MonoBehaviour {
+
+    public float speed = 5f;
+    float x, z;
+    
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        z = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+        x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+
+        transform.position += new Vector3(x, 0f, z);
+    }
+}
